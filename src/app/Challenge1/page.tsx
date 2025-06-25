d29ae4841a95898889400f7e3a9d35544242630d
STHEXA <ti.syu.s@gmail.com>
2025-06-25 22:01:00 +0900

    ボタンを押してAPI取得

--- src/app/Challenge1/page.tsx
+++ src/app/Challenge1/page.tsx
@@ -1,13 +1,8 @@
-import UserList from "@/components/UserList";
 import UserListBtn from "@/components/UserListBtn";
 
-export default function Home() {
+export default function Challenge1Page() {
   return (
     <div className="flex justify-center flex-col items-center">
-      <h2 className="font-medium text-gray-600 text-6xl m-3">
-        ユーザー一覧取得
-      </h2>
-      <UserList />
       <h2 className="font-medium text-gray-600 text-6xl m-3">
         ユーザー一覧取得ボタンで
       </h2>
---
+++ src/components/UserListBtn.tsx
@@ -0,0 +1,88 @@
+"use client";
+import { useState } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
+export default function UserList() {
+  const [userList, setUserList] = useState<User[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const getFetchData = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      if (!res.ok) {
+        throw new Error("ユーザー一覧が取得できませんでした。");
+      }
+      const data = await res.json();
+      setUserList(data);
+    } catch (e) {
+      if (e instanceof Error) {
+        setError(e.message);
+      } else {
+        setError("不明なエラーが発生しました。");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <button onClick={getFetchData} className="p-2.5 bg-gray-400 rounded-md">
+        ユーザーを取得する
+      </button>
+      {loading && <p>読込中・・・</p>}
+      {error && <p>{error}</p>}
+      {userList && (
+        <>
+          <p className="text-4xl mt-4 mb-4">APIで取得したユーザー一覧です。</p>
+          <ul>
+            {userList &&
+              userList.map((user) => (
+                <li key={user.id}>
+                  <p>
+                    id: {user.id} / 名前: {user.name} / ユーザー名:{" "}
+                    {user.username}
+                  </p>
+                  <p>メアド：{user.email}</p>
+                  <p className="text-2xl">住所</p>
+                  <p>郵便番号：{user.address.zipcode}</p>
+                  <p>街：{user.address.city}</p>
+                  <a
+                    href={user.website}
+                    className="text-blue-300 hover:opacity-70"
+                  >
+                    {user.website}
+                  </a>
+                </li>
+              ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}

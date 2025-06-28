e886b73128b3bc4870daea83fc45b5ca753aaea8
STHEXA <ti.syu.s@gmail.com>
2025-06-28 16:37:40 +0900

    axiosを使う形式に変更

---
+++ src/app/Challenge4/page.tsx
@@ -0,0 +1,12 @@
+import UserListAxios from "@/components/UserListAxios";
+
+export default function Challenge2Page() {
+  return (
+    <div className="flex justify-center flex-col items-center">
+      <h2 className="font-medium text-gray-600 text-6xl m-3">
+        ユーザー一覧 絞りこみ検索 axios使用
+      </h2>
+      <UserListAxios />
+    </div>
+  );
+}

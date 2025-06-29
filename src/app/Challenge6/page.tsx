c78d1ed4eb388c78e9899ed9af3a039b8d4faf9e
STHEXA <ti.syu.s@gmail.com>
2025-06-29 16:55:13 +0900

    削除文言を追加

--- src/app/Challenge6/page.tsx
+++ src/app/Challenge6/page.tsx
@@ -4,7 +4,7 @@ export default function Challenge6Page() {
   return (
     <div className="flex flex-col items-center justify-center">
       <h2 className="m-3 text-6xl font-medium text-gray-600">
-        ユーザー投稿　POST
+        ユーザー投稿　POSTを削除
       </h2>
       <UserListDelete />
     </div>
